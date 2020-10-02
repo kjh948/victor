716a6158008063ea7bd538a08a2e55ab38a2cedd
kjh948@gmail.com <kjh948@gmail.com>
2020-10-02 15:39:21 +0100

    esp12 interface

---
+++ rp/esppi/robotbase.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import serial
+import struct
+import sys
+import time
+import math
+import numpy as np
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+def serialWriteNumToByte(token, var=[]): # Only to be used for c m u b i l o within Python
+    #print("Num Token "); print(token);print(" var ");print(var);print("\n\n");
+    if token =='c' or token =='m' or token =='u' or token =='b':
+        instrStr = token + ":" + str(var[0])+" "+str(var[1])+'\n'
+    print "!!!!"+ instrStr
+    ser.write(instrStr)
+
+if __name__ == '__main__':
+    while 1:
+        serialWriteNumToByte('m',[10000,0])
+        time.sleep(0.1)
+        serialWriteNumToByte('m',[0,0])
+        time.sleep(0.1)
+        serialWriteNumToByte('m',[0,10000])
+        time.sleep(0.1)
